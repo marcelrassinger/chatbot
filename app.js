69f90a0ef38125f88b7bd1d6af26ede03d3d697f
marcelrassinger <rassinger@archilogic.ch>
2016-04-22 17:50:30 +0200

    First BotConnectorBot having valid credentials

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
+var restify = require('restify');
 var builder = require('botbuilder');
 
-var helloBot = new builder.TextBot();
-helloBot.add('/', function (session) {
-    if (!session.userData.name) {
-        session.beginDialog('/profile');
-    } else {
-        session.send('Hello %s!', session.userData.name);
-    }
+// Create bot and add dialogs
+// Parameters from https://dev.botframework.com/#/bots?id=GettingStartedSampleBot
+var bot = new builder.BotConnectorBot({ appId: 'GettingStartedSampleBot', appSecret: 'd3596c26a44d4057bf3a8b525a650051' });
+bot.add('/', function (session) {
+    session.send('Hello World');
 });
-helloBot.add('/profile', [
-    function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        session.endDialog();
-    }
-]);
 
-helloBot.listenStdin();
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
