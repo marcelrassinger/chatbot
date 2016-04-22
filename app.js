4f36ea67bb6dc3f486f207cb3246201fa2581ef8
marcelrassinger <rassinger@archilogic.ch>
2016-04-22 18:39:21 +0200

    Extended simple bot with command dialog

--- app.js
+++ app.js
@@ -4,9 +4,30 @@ var builder = require('botbuilder');
 // Create bot and add dialogs
 // Parameters from https://dev.botframework.com/#/bots?id=GettingStartedSampleBot
 var bot = new builder.BotConnectorBot({ appId: 'GettingStartedSampleBot', appSecret: 'd3596c26a44d4057bf3a8b525a650051' });
-bot.add('/', function (session) {
-    session.send('Hello World');
-});
+
+bot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            session.send('Hello %s!', session.userData.name);
+        }
+    }));
+bot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
